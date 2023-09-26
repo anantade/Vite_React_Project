8f87bec7123936481b559bb1cc4a3ac9c0d0c479
Anant TAde <anantade1992@gmail.com>
2023-09-26 18:06:03 +0530

    Forth commit

--- src/Components/Blog/Blog.jsx
+++ src/Components/Blog/Blog.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Navbar from '../Home/Navbar'
+import Blogimage from './Blogimage'
 
 export default function Blog() {
   return (
     <>
     <Navbar />
+    <Blogimage />
     </>
   )
 }
--- src/Components/Blog/Blogimage.jsx
+++ src/Components/Blog/Blogimage.jsx
@@ -3,7 +3,12 @@ import React from 'react'
 export default function Blogimage() {
   return (
     <div>
-      
+      <div className='bg-[url("https://www.telekom.com/resource/image/530616/landscape_ratio2x1/3000/1500/c3e6730fcd3f9223420a2ab53db0ede4/E85A54949CB431F3D954D9FC51DFA799/bi-blogschmuckbild-en.jpg")] md:block hidden bg-cover  h-[550px] w-[100%] '>
+       <div className='pt-44 pl-20 pb-[279px] bg-gradient-to-r from-black/100 from-25% via-black/95 via-45% to-black/30 to-70% '>
+        <p className='text-white text-3xl'>Blog</p>
+        <p className='text-white text-sm '>Lorem ipsum dolor, sit amet consectetur <br />adipisicing elit. Facilis illo quae delectus eius, <br />nesciunt ipsa!</p>
+       </div>
+      </div>
     </div>
   )
 }
